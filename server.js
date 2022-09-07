707661f1b2188e052087118360764cad0be4d0f0
Michael Miller <millergm30@gmail.com>
2022-09-07 21:15:16 -0230

    center error message

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride("_method"));
-app.use(express.static(__dirname + "public"));
 app.get("/", checkAuthenticated, (req, res) => {
   res.render("index.ejs", { name: req.user.name });
 });
